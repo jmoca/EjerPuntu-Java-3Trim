c01c64051354908dd211a156b4f2050ff9f651c5
JJ <jmorenocaballero@safareyes.es>
2024-03-21 13:07:11 +0100

    d

---
+++ CambiaVocales.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class CambiaVocales {
+
+    // despues cuando see lea en pantalla en la lectura poner original -> cambiado
+    public static void main(String[] args) {
+        CambiaVocales cv = new CambiaVocales();
+        System.out.println("PROGRAMA CAMBIAVOCALES\n");
+        cv.texto();
+    }
+    public void texto(){
+        System.out.println("Introduzca el texto:");
+        Scanner teclado = new Scanner(System.in);
+        
+
+    }
+    public void intro_Fic(){
+
+        System.out.println("oo00 Las líneas se han grabado en el fichero 00oo");
+    }
+    public void saca_Fic(){
+
+    }
+}
